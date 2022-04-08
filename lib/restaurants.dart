2620bdbd74022bb3df49c2cca31253f1b2c83bd3
Philip <philip_lorenz@icloud.com>
2022-04-08 10:22:23 +0200

    added function to display data in a formatted way

--- lib/restaurants.dart
+++ lib/restaurants.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class Restaurants extends StatelessWidget {
@@ -7,10 +9,16 @@ class Restaurants extends StatelessWidget {
   Restaurants({this.json});
   @override
   Widget build(BuildContext context) {
+    Map<String, dynamic> map = jsonDecode(json); // import 'dart:convert';
+    var _list = map.values.toList();
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text("${json!}"),
+      appBar: AppBar(
+        title: Text("TEST"),
+      ),
+      body: Container(
+        child: Column(
+          children: [for (var i in _list) Text(i["Telefon"])],
+        ),
       ),
     );
   }
